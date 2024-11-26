a63ee60b5d0ce231238c2fa723db31977da98e25
Vhile <blackfyrewm@gmail.com>
2024-11-26 21:48:50 +0100

    Fourth iteration refactored
    -Place shouting greetings to a separate function

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ function Greet(names){
     if (names.length == 0) {
         return "Hello, my friend.";
     }
+    if (names[0] == names[0].toUpperCase()) {
+        return ShoutingGreet(names);
+    }
     if (names.length == 1) {
-        if (names[0] == names[0].toUpperCase()) {
-            return "HELLO "+names+"!";
-        }
         return "Hello "+names+".";
     }
     else {
@@ -16,9 +16,21 @@ function Greet(names){
             if (i != names.length-2) s += ", ";
         }
         s += " and "+names[names.length-1]+".";
-        if (names[0] == names[0].toUpperCase()) {
-            s = s.toUpperCase().replace(".", "!");
+        return s
+    }
+}
+
+function ShoutingGreet(names) {
+    if (names.length == 1) {
+        return "HELLO "+names+"!";
+    }
+    else {
+        s = "HELLO ";
+        for (let i = 0; i < names.length-1; i++) {
+            s += names[i];
+            if (i != names.length-2) s += ", ";
         }
+        s += " AND "+names[names.length-1]+"!";
         return s
     }
 }
